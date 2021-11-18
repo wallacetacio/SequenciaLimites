478e92df0f010b317a667787b47250dd05f51313
Wallace Tácio <wallacett.dev@gmail.com>
2021-11-18 00:02:59 -0300

    Adiciona resolução ao exercício Sequência Limite ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SequenciaLimites
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            int inicio;
+
+            int fim;
+
+            Console.Write("Inicio: ");
+            inicio = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Fim: ");
+            fim = Convert.ToInt32(Console.ReadLine());
+
+            if(inicio > fim)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("ERRO! ");
+                Console.ResetColor();
+                Console.WriteLine("O número de inicio deve ser menor que o número de fim.");
+            }else
+            {
+                while (inicio <= fim)
+                {
+                    Console.Write($" {inicio} ");
+                    inicio = inicio + 1;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# SequenciaLimites
-Exercício Sequência Limites ETEC 2021
+## Exercício `SequenciaLimites` ETEC 2021
+
+Receba 2 números inteiros. Se o segundo for menor que o primeiro, exibir uma mensagem de erro. Caso contrário, exibir todos os números inteiros entre eles (inclusive).
+
+Ex.:
+```
+Início: -3
+Fim: 7
+-3 -2 -1 0 1 2 3 4 5 6 7
+```
